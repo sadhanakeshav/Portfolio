8ba263e0d7c5610c565e62d97755725de903060c
K Sadhana <sadhana.ggk@gmail.com>
2026-04-09 15:51:33 +0530

    Added Customer Churn Analytics project and updated README

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,7 +20,7 @@ import satsure from "./company/satsure.png";
 
 import hospital from "./hospital.jpg";
 import superstore from "./superstore.jpg";
-import beauty from ".churn.jpg";
+import beauty from "./churn.jpg";
 import plane from "./plane.jpg";
 import bank from "./bank.jpg";
 
